75ef509a67f53d63fa3999d40daa332c3aa2438e
Mansur Akidah <mansurakidah@gmail.com>
2023-07-05 20:11:09 +0300

    Add project files.

---
+++ SACCO System/Pages/Users/register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SACCO_System.Pages.Users
+{
+    public class registerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
