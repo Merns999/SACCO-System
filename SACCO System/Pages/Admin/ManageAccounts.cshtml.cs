8a0229042e60e20c0a4925af58e3d2c7bb46831a
Mansur Akidah <mansurakidah@gmail.com>
2023-07-09 10:48:01 +0300

    trying to implement js

---
+++ SACCO System/Pages/Admin/ManageAccounts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SACCO_System.Pages.Admin
+{
+    public class ManageAccountsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
