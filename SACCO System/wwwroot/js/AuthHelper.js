ef4bb5b670ff194a1867fcc3a81d6b8017bb56a5
Mansur Akidah <mansurakidah@gmail.com>
2023-07-10 20:30:54 +0300

    Js method to validate and register type of savings structured but not implemented

--- SACCO System/wwwroot/js/AuthHelper.js
+++ SACCO System/wwwroot/js/AuthHelper.js
@@ -1,1 +1,14 @@
-﻿
+﻿var AuthHelper = {
+    navigateToPath: function (url) {
+        debugger;
+        if (navigator.userAgent.match(/MSIE\s(?!9.0)/)) {
+            var referLink = document.createElement("a");
+            referLink.href = url;
+            document.body.appendChild(referLink);
+            referLink.click();
+        }     //all other browsers
+        else {
+            window.location.replace(url);
+        }
+    }
+    }
