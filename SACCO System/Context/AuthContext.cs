66efb185ba2d4a2c90479a1816472f174f59be19
Mansur Akidah <mansurakidah@gmail.com>
2023-07-19 19:43:17 +0300

    To be deleted

---
+++ SACCO System/Context/AuthContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SACCO_System.Data;
+
+namespace SACCO_System.Context
+{
+    public class AuthContext : IdentityDbContext
+    {
+        public AuthContext(DbContextOptions<SharesidSaccoContext> options) : base(options)
+        {
+        }
+    }
+}
